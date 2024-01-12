59d4d782dc82ecd20b400ad208232ea243908290
Artsiom Kosmin <artyom17may@gmail.com>
2024-01-12 12:15:09 +0300

    features branch changes

--- app/src/main/java/com/example/andersen_p2/MainActivity.kt
+++ app/src/main/java/com/example/andersen_p2/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name! Features branch changes",
         modifier = modifier
     )
 }
